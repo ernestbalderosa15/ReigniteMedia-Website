3d621f39b128413250d1e071abb24e9684b17693
Ernest Y. Balderosa <sofia_balderosa@dlsu.edu.ph>
2025-02-03 08:02:30 -0600

    Updated info
    -- v1.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://ReigniteMediatheme.vercel.app",
   integrations: [tailwind()],
   output: 'server',
   adapter: vercel(),
